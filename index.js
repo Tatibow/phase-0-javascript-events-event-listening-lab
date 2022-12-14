d736c2188934fa7e8ddfb07dc5cfdb1c5f196209
Tatiana <Bowmantatiana2@gmail.com>
2022-12-14 14:59:04 -0800

    commit tree 3b27be8061f2187ed671e364f00378f69f771c36

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
-}
+const input = document.getElementById("button");
+
+function addingEventListener(e) {
+    alert("I was clicked!");
+};
+
+input.addEventListener("click", addingEventListener);
